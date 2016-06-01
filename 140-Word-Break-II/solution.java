7476677b56956f2fa78865a73533b8a2ffe5d3ef
Binghong Fu <fubUmich@users.noreply.github.com>
2016-06-01 01:08:55 -0700

    [140. Word Break II][Accepted]committed by LeetCode Extension

--- 140-Word-Break-II/solution.java
+++ 140-Word-Break-II/solution.java
@@ -6,12 +6,12 @@ public class Solution {
         
         List<String> result = new ArrayList<String>();
 
-        for(int i = 1; i < s.length(); ++i){
-            if(wordDict.contains(s.substring(0, i))){
-                List<String> next = wordBreak(s.substring(i, s.length()), wordDict);
+        for(int i = s.length() - 1; i > 0; --i){
+            if(wordDict.contains(s.substring(i, s.length()))){
+                List<String> next = wordBreak(s.substring(0, i), wordDict);
                 if(next.size() > 0){
                     for(Iterator it = next.iterator(); it.hasNext();){
-                        result.add(s.substring(0, i) + " " + it.next());
+                        result.add(it.next() + " " + s.substring(i, s.length()));
                     }
                 }
             }
