50f60a801f6b63e20dd9606cb50266b7b182dec4
Binghong Fu <fubUmich@users.noreply.github.com>
2016-06-02 15:42:07 -0700

    [78. Subsets][Accepted]committed by LeetCode Extension

--- 78-Subsets/solution.py
+++ 78-Subsets/solution.py
@@ -4,17 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        nums.sort()
         subsets = []
-        self.get_all_subsets(nums, 0, [], subsets)
+        self.get_all_subsets(sorted(nums), 0, [], subsets)
         return subsets
         
     @staticmethod
-    def get_all_subsets(nums, idx, current_subset, subsets):
-        subsets.append(list(current_subset))
-        while idx < len(nums):
-            current_subset.append(nums[idx])
-            Solution.get_all_subsets(nums, idx + 1, current_subset, subsets)
-            current_subset.pop()
-            idx += 1
-        
+    def get_all_subsets(nums, start, current_subset, subsets):
+        subsets.append(current_subset)
+        for i in xrange (start, len(nums)):
+            Solution.get_all_subsets(nums, i + 1, current_subset + [nums[i]], subsets)
