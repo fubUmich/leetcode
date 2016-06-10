b8f5420ce427da74469f1bb9732ff3aa98a57287
Binghong Fu <fubUmich@users.noreply.github.com>
2016-06-10 00:37:01 -0700

    [354. Russian Doll Envelopes][Accepted]committed by LeetCode Extension

---
+++ 354-Russian-Doll-Envelopes/solution.java
@@ -0,0 +1,42 @@
+public class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes, new Comparator<int[]>(){
+            @Override
+            public int compare(int[] arr1, int[] arr2){
+                if(arr2[0] == arr1[0])
+                    return arr2[1] - arr1[1];
+                return arr1[0] - arr2[0];
+            }
+        });
+        
+        
+        
+        List<Integer> dp = new ArrayList<Integer>();
+        for(int[] env : envelopes){
+            System.out.print(env[0]);
+            System.out.print(env[1]);
+            System.out.println();
+            int index = binary_search(dp, env[1]);
+            if(index == dp.size()){
+                dp.add(env[1]);
+            }
+            else
+                dp.set(index, env[1]);
+        }
+        
+        return dp.size();
+    }
+    
+    private int binary_search(List<Integer> list, int target){
+        int start = 0, end = list.size() - 1, mid = start + (end - start) / 2;
+        while(start <= end){
+            if(list.get(mid) < target)
+                start = mid + 1;
+            else
+                end = mid - 1;
+            mid = start + (end - start) / 2;
+        }
+        return start;
+    }
+}
+
